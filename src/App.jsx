5482ad4c7b2b25a36f4902aeb688034ce540fc77
codeofmj <mjl6005@gmail.com>
2026-04-23 14:52:25 +0900

    button add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const handleClick = async () => {
+    const response = await axios.get("/board/list");
+    console.log(response.data);
+  };
   return (
     <>
       <h1>CI/CD 자동 배포 테스트 완료!</h1>
+      <button onClick={handleClick}>데이터 가져오기</button>
     </>
   );
 }
